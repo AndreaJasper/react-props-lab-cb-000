0e274c70ff588cc849b6af7a050c41d317c5e5e8
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 15:04:40 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,5 +1,12 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const name = ['Millennium Falcon', 'TIE Fighter', 'X-Wing', 'Separatist Dreadnought', 'Imperial Shuttle']
+
+Spaceeship.defaultProps = {
+  speed: 'slow',
+  hasRockets: 'false',
+  colors: ['black', 'red']
+};
 
 
