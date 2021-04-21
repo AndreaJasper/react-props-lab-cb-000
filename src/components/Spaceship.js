a9bfcddccdd4e47f16387745267a3c41bc3786fa
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 15:10:50 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Spaceship extends React.Component {
-  const name = ['Millennium Falcon', 'TIE Fighter', 'X-Wing', 'Separatist Dreadnought']
+  const name = ['Millennium Falcon', 'TIE Fighter', 'X-Wing', 'Separatist Dreadnought', 'Imperial Shuttle']
   const speed = ['slow', 'lightspeed', 'moderate']
   const hasRockets = ['yes', 'no']
   const colors = ['silver', 'black', 'red', 'orange']
