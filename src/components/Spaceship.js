cd5402f31688341eada7c0e7a7e7ffa68ebe1ec9
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 15:22:58 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-class Spaceship extends React.Component {
-  render() {
+export default class Spaceship extends React.Component {
+  
+  render () {
     return (
-      <div className = "spaceship-card">
-        <h3>{this.props.name}</h3>
-        <p>Speed: {this.props.speed}<br />
-        Rockets? {this.props.hasRockets.toString()}<br />
-        Color Scheme: {this.props.colors.join(', ')}</p>
+      <div className="spaceship-card">
+        <h3>{this.props.name}</h3>  
+        <p>Speed: {this.props.speed}</p>
+        <p>Rockets {this.props.hasRockets.to_string()}</p>
+        <p>Colors: {this.props.colors.join(',')}</p>
       </div>
     )
   }
 }
-
-Spaceship.defaultProps = {
-  speed: 'slow',
-  hasRockets: false,
-  colors: ['black', 'red']
+  
+  Spaceship.defaultProps = {
+    speed: 'slow',
+    hasRockets: 'false',
+    colors: ['black', 'red']
+  }
 }
 
 export default Spaceship;
