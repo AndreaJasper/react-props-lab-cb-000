b451aa9b6858cb4cd2528a8c01944a46b15f30d1
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 15:08:50 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = ['Millennium Falcon', 'TIE Fighter', 'X-Wing', 'Separatist Dreadnought', 'Imperial Shuttle']
-const speed = ['slow', 'lightspeed', 'moderate']
-const hasRockets = ['yes', 'no']
-const colors = ['silver', 'black', 'red', 'orange']
-
-Spaceship.defaultProps = {
-  speed: 'slow',
-  hasRockets: 'false',
-  colors: ['black', 'red']
-};
-
+class Spaceship extends React.Component {
+  const name = ['Millennium Falcon', 'TIE Fighter', 'X-Wing', 'Separatist Dreadnought', 'Imperial Shuttle']
+  const speed = ['slow', 'lightspeed', 'moderate']
+  const hasRockets = ['yes', 'no']
+  const colors = ['silver', 'black', 'red', 'orange']
+  
+  Spaceship.defaultProps = {
+    speed: 'slow',
+    hasRockets: 'false',
+    colors: ['black', 'red']
+  };
+ 
+}
 
 
