b434aa1918a2b612e31ec8ec063864dee88cc2e7
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-21 15:03:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
-const name = ['Millennium Falcon', 'Tie Fighter']
-
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" />,
   document.getElementById('global')
